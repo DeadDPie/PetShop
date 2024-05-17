15775377809811b9ba04ad73cc1ffa81a000955a
DeadDPie <elizabethfox434@gmail.com>
2024-05-18 00:54:49 +0700

    сверстать шапку страницы

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -1,3 +1,22 @@
+import { Heart, Search, ShoppingCart, User } from "tabler-icons-react";
+
 export const Header = () => {
-  return <div className="w-full px-8 py-6 bg-primary">header</div>;
+  return (
+    <div className="w-full flex shadow-md gap-[55px]  justify-between items-center px-[15px] py-[10px] bg-white">
+      <div className="flex gap-[5px] items-center">
+        <img className="size-[30px]" src="./src/assets/logo.png" />
+        <span className="font-comfortaa text-primary ">ЦапЦарап</span>
+      </div>
+      <input
+        className="border-brown max-w-[735px] hidden smx:block placeholder-brown font-comfortaa placeholder-shown:text-center max-h-[45px] text-brown text-[22px] border-2 py-[10px] px-6 rounded-[40px] flex-grow"
+        placeholder="Поиск..."
+      />
+      <div className="flex gap-[17px] text-brown">
+        <Search className="sm:hidden" />
+        <Heart />
+        <User />
+        <ShoppingCart />
+      </div>
+    </div>
+  );
 };
