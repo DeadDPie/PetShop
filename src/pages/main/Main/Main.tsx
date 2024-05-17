cca879de739854b961cde1fb67bd8db5394005ef
DeadDPie <elizabethfox434@gmail.com>
2024-05-18 04:51:51 +0700

    добавить блок рекламы

--- src/pages/main/Main/Main.tsx
+++ src/pages/main/Main/Main.tsx
@@ -1,9 +1,16 @@
+import { Advertisement } from "./components/Advertisement/Advertisement";
 import { AnimalProducts } from "./components/AnimalProducts/AnimalProducts";
 
 export const Main = () => {
   return (
     <div>
+      <div className="sm:hidden">
+        <Advertisement />
+      </div>
       <AnimalProducts />
+      <div className="hidden sm:block">
+        <Advertisement />
+      </div>
     </div>
   );
 };
--- src/pages/main/Main/components/AnimalProducts/AnimalProducts.tsx
+++ src/pages/main/Main/components/AnimalProducts/AnimalProducts.tsx
@@ -1,21 +1,19 @@
 import { ANIMALS } from "@/assets/constants/animals";
 import { CategoryCard, Typography } from "@/shared/ui";
 
-export const AnimalProducts = () => {
-  return (
-    <div className="flex flex-col items-center pt-[21px] xl:pt-[68px] px-6 pb-[3px] xl:gap-[25px] gap-[8px]">
-      <Typography variant="h4" className="xl:text-[32px]">
-        Товары для животных
-      </Typography>
-      <div className="grid grid-cols-3 sm:grid-cols-5 xl:gap-x-7 gap-x-[8px] gap-y-6 ">
-        {ANIMALS.map((animal) => (
-          <CategoryCard
-            image={animal.image}
-            route={animal.route}
-            title={animal.title}
-          />
-        ))}
-      </div>
+export const AnimalProducts = () => (
+  <div className="flex flex-col items-center pt-[21px] xl:pt-[68px] px-6 pb-[3px] xl:gap-[25px] gap-[8px]">
+    <Typography variant="h4" className="xl:text-[32px]">
+      Товары для животных
+    </Typography>
+    <div className="grid grid-cols-3 sm:grid-cols-5 xl:gap-x-7 gap-x-[8px] gap-y-6 ">
+      {ANIMALS.map((animal) => (
+        <CategoryCard
+          image={animal.image}
+          route={animal.route}
+          title={animal.title}
+        />
+      ))}
     </div>
-  );
-};
+  </div>
+);
