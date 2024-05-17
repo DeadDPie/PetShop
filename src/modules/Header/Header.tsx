d604d60482a37fea272189ee391159743ecca728
DeadDPie <elizabethfox434@gmail.com>
2024-05-18 01:12:58 +0700

    поправить верстку

--- src/modules/Header/Header.tsx
+++ src/modules/Header/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
         <span className="font-comfortaa text-primary ">ЦапЦарап</span>
       </div>
       <input
-        className="border-brown max-w-[735px] hidden smx:block placeholder-brown font-comfortaa placeholder-shown:text-center max-h-[45px] text-brown text-[22px] border-2 py-[10px] px-6 rounded-[40px] flex-grow"
+        className="border-brown max-w-[735px] hidden sm:block placeholder-brown font-comfortaa placeholder-shown:text-center max-h-[45px] text-brown text-[22px] border-2 py-[10px] px-6 rounded-[40px] flex-grow"
         placeholder="Поиск..."
       />
       <div className="flex gap-[17px] text-brown">
