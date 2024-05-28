393bdeb0dc1e0deafdd3489e7447d04ac17660c4
Subbotina Olesya <elizabethfox434@gmail.com>
2024-05-29 01:40:08 +0700

    Review вёрстка

--- src/pages/main/AccountPage/Review.tsx
+++ src/pages/main/AccountPage/Review.tsx
@@ -1,6 +1,8 @@
 import { ProductProps } from "@/shared/api/types/types";
 import { Button, Typography } from "@/shared/ui";
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { Star } from "tabler-icons-react";
 
 interface ReviewProps {
   setModalReview: (props: boolean) => void;
@@ -8,6 +10,7 @@ interface ReviewProps {
 }
 
 export const Review = ({ setModalReview, product }: ReviewProps) => {
+  const [rating, setRating] = useState(0);
   return (
     <div className="bg-white px-8 rounded-[20px] min-w-[300px] max-w-[330px] border-white py-[30px] flex flex-col  justify-center xl:p-16 lg:min-w-[520px]">
       <Typography
@@ -19,15 +22,15 @@ export const Review = ({ setModalReview, product }: ReviewProps) => {
 
       <form>
         <div className="flex flex-col xl:flex-row">
-          <div className="flex flex-col items-center justify-between gap-[3px] w-[135px] h-[213px] xl:h-[299px]  xl:w-[210px]">
+          <div className="flex flex-col items-center justify-between gap-[3px] min-w-[100px] min-h-[180px] xl:h-[299px]  xl:w-[210px] my-4">
             <Typography
-              variant="h3"
-              className="font-bold xl:text-[24px] flex justify-center xl:mb-4"
+              variant="h4"
+              className="xl:text-[24px] mb-2 flex justify-center xl:mb-4"
             >
               {product.title}
             </Typography>
             {product.image && (
-              <div className="flex-grow w-full overflow-hidden relative cursor-pointer">
+              <div className="flex-grow w-full overflow-hidden relative cursor-pointer ">
                 <Link to={`/${product.id}`}>
                   <img
                     src={product.image}
@@ -38,23 +41,36 @@ export const Review = ({ setModalReview, product }: ReviewProps) => {
             )}
           </div>
 
-          <div className="m-2">
+          <div className="w-full">
+            <div className="relative flex items-center gap-[15px]">
+              {Array(5)
+                .fill(0)
+                .map((_, index) => (
+                  <Star
+                    key={`empty-${index}`}
+                    className={`text-yellow cursor-pointer ${
+                      index < rating ? "fill-yellow" : ""
+                    }`}
+                    onClick={() => setRating(index + 1)}
+                  />
+                ))}
+            </div>
             <input
               type="text"
               id="post"
-              className="w-full h-full p-2 border border-beige bg-beige rounded-xl"
+              className="min-w-[200px] w-full my-1 min-h-[200px] p-2 border border-beige bg-beige rounded-xl placeholder:text-center"
               placeholder="Здесь можно оставить отзыв"
             />
           </div>
         </div>
 
-        <div className="flex flex-row gap-5 justify-center xl:justify-around">
-          <Button type="submit" className="px-2 py-1">
+        <div className="flex flex-row gap-5 justify-between xl:justify-around mt-4">
+          <Button type="submit" className="px-1 py-[1px] xl:px-4 xl:py-2">
             Оставить отзыв
           </Button>
           <Button
             variant="OUTLINE"
-            className="px-2 py-1"
+            className="px-1 py-[1px]"
             onClick={() => setModalReview(false)}
           >
             Отмена
