ed668bec9a39904d2350932a5f9f5c63cf8d01e5
DeadDPie <elizabethfox434@gmail.com>
2024-12-21 11:45:33 +0700

    добавление в корзину

--- src/pages/main/CartPage/Cart.tsx
+++ src/pages/main/CartPage/Cart.tsx
@@ -2,23 +2,40 @@ import { PRODUCTS } from "@/mock/database/database";
 import { CartItem } from "./CartItem";
 import { Typography } from "@/shared/ui";
 import { useCart } from "@/shared/contexts/cart";
+import { useGetProductsQuery } from "@/pages/admin/hooks/useGetProductsQuery";
+import { useState } from "react";
 export const Cart = () => {
-  const { cart } = useCart();
-  return (
-    <div className="w-full max-w-[713px]">
-      <Typography
-        variant="h4"
-        className="flex justify-center mb-7 xl:text-[30px]"
-      >
-        Корзина
-      </Typography>
-      <div className="flex flex-col gap-x-[35px] gap-y-6">
-        {PRODUCTS.filter((product) =>
-          cart.some((item) => item.id === product.id)
-        ).map((product) => (
-          <CartItem {...product} />
-        ))}
-      </div>
-    </div>
-  );
+	const { cart } = useCart();
+	const [currentPage, setCurrentPage] = useState(1);
+	const limit = 10;
+
+	// Получаем данные о продуктах
+	const { data, isSuccess, isLoading, isError } = useGetProductsQuery({
+		params: {
+			limit: limit.toString(),
+			current: currentPage.toString(),
+		},
+	});
+	return (
+		<div className="w-full max-w-[713px]">
+			<Typography
+				variant="h4"
+				className="flex justify-center mb-7 xl:text-[30px]"
+			>
+				Корзина
+			</Typography>
+			<div className="flex flex-col gap-x-[35px] gap-y-6">
+				{/* {PRODUCTS.filter((product) =>
+					cart.some((item) => item.id === product.id)
+				).map((product) => (
+					<CartItem {...product} />
+				))} */}
+				{data?.rows
+					.filter((product) => cart.some((item) => item.id === product.id))
+					.map((product) => (
+						<CartItem {...product} />
+					))}
+			</div>
+		</div>
+	);
 };
--- src/pages/main/CartPage/CartItem.tsx
+++ src/pages/main/CartPage/CartItem.tsx
@@ -5,73 +5,73 @@ import { Button } from "@/shared/ui";
 import { CartProps, useCart } from "@/shared/contexts/cart";
 import { ProductProps } from "@/shared/api/types/types";
 export const CartItem = (product: ProductProps) => {
-  const { cart, setCart } = useCart();
-  const cartItem = cart.find((item) => item.id === product.id);
+	const { cart, setCart } = useCart();
+	const cartItem = cart.find((item) => item.id === product.id);
 
-  const changCart = (change: number) => {
-    if (change !== 0) {
-      setCart((prevCart: CartProps[]) => {
-        return prevCart.map((cartItem) => {
-          if (cartItem.id === product.id) {
-            cartItem.amount += change;
-          }
-          return cartItem;
-        });
-      });
-    } else {
-      setCart((prevCart: CartProps[]) => {
-        return prevCart.filter((cartItem) => cartItem.id !== product.id);
-      });
-    }
-  };
+	const changCart = (change: number) => {
+		if (change !== 0) {
+			setCart((prevCart: CartProps[]) => {
+				return prevCart.map((cartItem) => {
+					if (cartItem.id === product.id) {
+						cartItem.amount += change;
+					}
+					return cartItem;
+				});
+			});
+		} else {
+			setCart((prevCart: CartProps[]) => {
+				return prevCart.filter((cartItem) => cartItem.id !== product.id);
+			});
+		}
+	};
+	const SERVER_URL = "http://localhost:3000";
+	return (
+		<div className="w-full flex flex-row border rounded-xl justify-between border-greyDark p-2 md:p-4">
+			<div className="flex md:gap-[56px]">
+				<Link to={`/${product.id}`} className="relative cursor-pointer">
+					<img
+						src={`${SERVER_URL}${product.image}`}
+						alt={product.title}
+						className="w-38 max-w-36 h-26 md:pl-[30px] xl:w-42 xl:h-30 object-contain"
+					/>
+				</Link>
+				<div className="flex flex-col place-content-between">
+					<div className="flex-grow">
+						<p className="text-sm font-semibold text-wrap flex justify-start mt-[4px] lg:text-xl">
+							{product.title}
+						</p>
+					</div>
+					<div className="h-[29px] md:h-[40px] mb-1">
+						<button
+							className="text-sm px-[14px] h-full py-1 md:px-[20px] md:text-xl bg-grey border-2 border-grey rounded-l-[12px] hover:bg-greyDark"
+							onClick={() => changCart(-1)}
+							disabled={cartItem?.amount === 1}
+						>
+							-
+						</button>
+						<button className="text-sm px-[14px] h-full py-1 md:px-[20px] md:text-xl bg-white border-2 border-grey border-l border-r hover:bg-greyDark">
+							{cartItem?.amount}
+						</button>
+						<button
+							className="text-sm px-[14px] py-1 h-full md:px-[20px] md:text-xl bg-grey border-2 border-grey rounded-r-[12px] hover:bg-greyDark"
+							onClick={() => changCart(1)}
+						>
+							+
+						</button>
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-col place-content-between items-center pb-3">
+				<button>
+					<X></X>
+				</button>
 
-  return (
-    <div className="w-full flex flex-row border rounded-xl justify-between border-greyDark p-2 md:p-4">
-      <div className="flex md:gap-[56px]">
-        <Link to={`/${product.id}`} className="relative cursor-pointer">
-          <img
-            src={product.image}
-            alt={product.title}
-            className="w-38 h-26 md:pl-[30px] xl:w-42 xl:h-30 object-contain"
-          />
-        </Link>
-        <div className="flex flex-col place-content-between">
-          <div className="flex-grow">
-            <p className="text-sm font-semibold text-wrap flex justify-start mt-[4px] lg:text-xl">
-              {product.title}
-            </p>
-          </div>
-          <div className="h-[29px] md:h-[40px] mb-1">
-            <button
-              className="text-sm px-[14px] h-full py-1 md:px-[20px] md:text-xl bg-grey border-2 border-grey rounded-l-[12px] hover:bg-greyDark"
-              onClick={() => changCart(-1)}
-              disabled={cartItem?.amount === 1}
-            >
-              -
-            </button>
-            <button className="text-sm px-[14px] h-full py-1 md:px-[20px] md:text-xl bg-white border-2 border-grey border-l border-r hover:bg-greyDark">
-              {cartItem?.amount}
-            </button>
-            <button
-              className="text-sm px-[14px] py-1 h-full md:px-[20px] md:text-xl bg-grey border-2 border-grey rounded-r-[12px] hover:bg-greyDark"
-              onClick={() => changCart(1)}
-            >
-              +
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col place-content-between items-center pb-3">
-        <button>
-          <X></X>
-        </button>
-
-        <div className="felx justify-center text-base font-medium md:text-2xl ">
-          <p>{product.price}₽</p>
-        </div>
-      </div>
-    </div>
-  );
+				<div className="felx justify-center text-base font-medium md:text-2xl ">
+					<p>{product.price}₽</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 /*<Button
           variant="OUTLINE"
